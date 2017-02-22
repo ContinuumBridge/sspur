239a8befec49e4e81218b73513cf92bbf1ea1e91
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-22 23:23:28 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='sspur',
-      version='0.1.14',
+      version='0.1.15',
       author='Peter Claydon',
       author_email='peter.claydon@continuumbridge.com',
       py_modules=['sspur'],
