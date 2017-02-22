d04b1dbcbca943c0fb43dffa918fd4355e1bb3ef
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-22 11:00:12 +0000

    Testing

---
+++ snappy-spur.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# spur.py
+# Copyright (C) ContinuumBridge Limited, 2017 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Peter Claydon
+#
+import subprocess
+
+def main():
+    print("Snappy Spur Starting")
+    import bridge.manager.cbsupervisor
+
+if __name__ == '__main__':
+    main()
+
