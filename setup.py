e3d4254f5ba90735e34d2b348b4349dae0a1f726
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-22 17:14:56 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='sspur',
-      version='0.1.7',
+      version='0.1.8',
       author='Peter Claydon',
       author_email='peter.claydon@continuumbridge.com',
       packages=['bridge', 'apps_dev', 'adaptors_dev'],
-      py_modules=['sspur'],
       install_requires=[
           'Twisted',
           'pysftp',
