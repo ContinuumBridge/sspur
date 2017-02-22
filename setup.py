d5aaa99c0fb96d59f53f234471cddf7a3268b502
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-22 13:03:28 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
-setup(name='snappy-spur',
-      version='0.1.3',
+setup(name='spur',
+      version='0.1.4',
       author='Peter Claydon',
       author_email='peter.claydon@continuumbridge.com',
-      py_modules=['snappy-spur'],
+      py_modules=['spur'],
       install_requires=[
           'Twisted',
           'pysftp',
@@ -15,7 +15,7 @@ setup(name='snappy-spur',
       ],
       entry_points={
           'console_scripts': [
-              'snappy-spur=snappy-spur:main'
+              'spur = spur:main'
            ]
       },
       )
