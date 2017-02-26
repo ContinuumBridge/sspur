fb611c16296cc3d79aec0d5f642ff30e22950e43
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-26 18:02:38 +0000

    Added to keep everything in one place

---
+++ thisbridge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This file should be called:
+# ~/snap/sspur/common/thisbridge/thisbridge.sh
+# Note that it needs to be executable
+#
+export CB_DJANGO_CONTROLLER_ADDR="portal.continuumbridge.com"
+export CB_NODE_CONTROLLER_ADDR="portal.continuumbridge.com"
+export CB_BRIDGE_KEY='434f3139LkQc/uTaYWA6LmiOmcJfu+FtQmdIdfMoroOctEKDxWR9/i+7BHJLx3px'
+export CB_BRIDGE_URI='/api/bridge/v1/bridge/273'
+export CB_BRIDGE_NAME=''
+export CB_BID='BID273'
+export CB_PERIPHERALS='LPRS Adaptor'
+export CB_LOG_ENVIRONMENT="DEBUG"
+export CB_DEV_BRIDGE=True
+export CB_DEV_APPS="Spur App, Adaptor Test App"
+export CB_DEV_ADAPTORS="LPRS Adaptor"
+export CB_USERNAME="bridge"
+export CB_ZWAVE_PASSWORD="t00f@r"
+export CB_RASPBERRY='False'
+
